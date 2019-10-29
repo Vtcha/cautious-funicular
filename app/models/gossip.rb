c8a86ff588e51e0444335fb25dd06afa22c2f8d4
Vtcha <55757241+Vtcha@users.noreply.github.com>
2019-10-29 09:23:00 +0100

    Update gossip.rb

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -2,8 +2,5 @@ class Gossip < ApplicationRecord
 	belongs_to :user, required: false
 	belongs_to :tag_gossip, required: false
 	has_many :tags, through: :tag_gossips
-	has-many :comments
 
-	validates :title, presence: true, length: {minimum: 3, maximum: 14, message: "title need 3 car. or less than 14 car"}
-	validates : content,presence: true
 end
